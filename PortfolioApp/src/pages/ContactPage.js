6115a18c8d330b698866b9356dec942b6065515a
Diego-Cano <diegocano_1999@hotmail.com>
2024-05-06 20:23:26 -0700

    Extend Personal Portfolio App with Expo Image Picker
    Assignment

--- PortfolioApp/src/pages/ContactPage.js
+++ PortfolioApp/src/pages/ContactPage.js
@@ -1,61 +1,56 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, KeyboardAvoidingView, Platform, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const ContactPage = () => {
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [message, setMessage] = React.useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [imageUri, setImageUri] = useState(null);  // State to store the URI of the selected image
 
-  // Function to handle form submission
+  useEffect(() => {
+    // Requesting permission to access the photo library
+    (async () => {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to access camera roll is required!');
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,  // Allows the user to crop the image
+      quality: 1,  // High quality image
+    });
+
+    if (!result.cancelled) {
+      setImageUri(result.assets[0].uri);
+    }
+  };
+
   const handleSubmit = () => {
     alert('Thank you for reaching out!');
   };
 
-  // Image to display at the bottom
-  const bottomImage = require('./newProfile.png');
-
   return (
     <ScrollView style={styles.scrollView}>
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
-        
         <View style={styles.titleBox}>
           <Text style={styles.title}>Get In Touch</Text>
         </View>
-
         <View style={styles.formContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Your Name"
-            value={name}
-            onChangeText={setName}
-            placeholderTextColor="#666"
-          />
-          
-          <TextInput
-            style={styles.input}
-            placeholder="Your Email"
-            value={email}
-            onChangeText={setEmail}
-            keyboardType="email-address"
-            placeholderTextColor="#666"
-          />
-
-          <TextInput
-            style={[styles.input, styles.inputMessage]}
-            placeholder="Your Message"
-            value={message}
-            onChangeText={setMessage}
-            multiline={true}
-            numberOfLines={4}
-            placeholderTextColor="#666"
-          />
-
+          <TextInput style={styles.input} placeholder="Your Name" value={name} onChangeText={setName} placeholderTextColor="#666" />
+          <TextInput style={styles.input} placeholder="Your Email" value={email} onChangeText={setEmail} keyboardType="email-address" placeholderTextColor="#666" />
+          <TextInput style={[styles.input, styles.inputMessage]} placeholder="Your Message" value={message} onChangeText={setMessage} multiline={true} numberOfLines={4} placeholderTextColor="#666" />
+          {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+          <TouchableOpacity onPress={pickImage} style={styles.linkButton}>
+            <Text style={styles.linkText}>- Click here to Upload an Image -</Text>
+          </TouchableOpacity>
           <TouchableOpacity onPress={handleSubmit} style={styles.button}>
             <Text style={styles.buttonText}>Send Message</Text>
           </TouchableOpacity>
         </View>
-
-        <Image source={bottomImage} style={styles.image} />
       </KeyboardAvoidingView>
     </ScrollView>
   );
@@ -63,11 +58,11 @@ const ContactPage = () => {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#f0f8ff', // Light blue background
+    backgroundColor: '#f0f8ff',
   },
   container: {
     flex: 1,
-    justifyContent: 'center', // Center content vertically in the screen
+    justifyContent: 'center',
     padding: 20
   },
   titleBox: {
@@ -75,66 +70,74 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 6,
-    backgroundColor: '#4682B4', // Changed to light calming blue
+    backgroundColor: '#4682B4',
     borderRadius: 10,
     paddingVertical: 12,
     paddingHorizontal: 20,
     marginVertical: 20,
-    alignItems: 'center', // Center title text horizontally
-    alignSelf: 'stretch', // Stretch to the container width
+    alignItems: 'center',
+    alignSelf: 'stretch',
   },
   title: {
     fontSize: 26,
     fontWeight: 'bold',
-    color: '#ffffff', // White text for button
+    color: '#ffffff',
   },
   formContainer: {
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 6,
-    backgroundColor: '#ffffff', // White background for the form
+    backgroundColor: '#ffffff',
     borderRadius: 10,
     padding: 20,
-    alignItems: 'stretch', // Stretch children to match width
+    alignItems: 'stretch',
   },
   input: {
-    backgroundColor: '#fff', // White background for inputs
+    backgroundColor: '#fff',
     borderWidth: 1,
-    borderColor: '#ccc', // Light grey border
+    borderColor: '#ccc',
     borderRadius: 8,
     padding: 12,
     marginBottom: 12,
     fontSize: 16,
   },
   inputMessage: {
-    minHeight: 100, // Larger input area for the message
+    minHeight: 100,
   },
   button: {
-    backgroundColor: 'transparent', // Transparent background
-    borderWidth: 2, // Solid border to denote button edges
-    borderColor: '#003366', // Dark blue border color
+    backgroundColor: '#f0f8ff', // Light blue background
+    borderWidth: 2,
+    borderColor: '#003366',
     borderRadius: 8,
-    paddingVertical: 8, // Padding to keep the text centered vertically
-    paddingHorizontal: 20, // Padding to keep the text from being too cramped
-    width: 200, // Set a fixed width for uniformity
-    alignItems: 'center', // Center text within the button
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    width: 200,
+    alignItems: 'center',
     marginTop: 10,
-    alignSelf: 'center', // Ensure the button is centered within its container
+    alignSelf: 'center',
   },
-  
   buttonText: {
-    fontSize: 16, // Font size for readability
-    color: '#003366', // Dark blue text to match the border
+    fontSize: 16,
+    color: '#003366',
     fontWeight: 'bold',
-  }
-  ,
-
+  },
+  linkButton: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  linkText: {
+    color: '#DC143C',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
   image: {
-    width: '100%', // Full width to match the form's width
-    height: 200, // Fixed height
+    width: '100%',
+    height: 200,
     marginTop: 20,
-    borderRadius: 10, // Rounded corners for the image
+    marginBottom: 20,
+    borderRadius: 10,
   }
 });
 
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
  App dev class
 
  In developing the app, I focused on ensuring a visually cohesive and professional design across all pages, using various shades of blue to evoke a sense of calm. I chose minimalist styles for buttons and forms, enhancing usability with subtle design elements like shadow effects and rounded corners. Balancing aesthetics with functionality posed a challenge, particularly in aligning design elements across different pages. Through iterative design and testing, I learned the importance of detail in UI design and the value of maintaining a consistent theme while adapting to usability needs.
+
+_________
+
+In redesigning the ContactPage for my portfolio app, I aimed to enhance user interaction while maintaining a visually appealing interface. I implemented an image picker feature that allows users to easily upload images from their devices, making their interactions more personalized. 
+
+ I encountered several challenges, particularly in ensuring that images selected by users were displayed correctly and promptly. Adapting the app for consistent functionality across both iOS and Android platforms required detailed adjustments, especially in managing access permissions with Expo's Image Picker. I focused on balancing these functional changes with a streamlined design, which involved special layout planning and iterative testing. This approach was crucial to refining the app’s functionality and improving the overall user experience, ensuring the interface remained simple and effective for all users.
